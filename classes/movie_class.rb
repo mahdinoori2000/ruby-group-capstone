0962482b611d9fdfafa164d02cbe12a5de03e861
mariegrace <mariegracebmg@gmail.com>
2023-09-26 13:25:12 +0200

    Add the movie properties

--- classes/movie_class.rb
+++ classes/movie_class.rb
@@ -1,4 +1,17 @@
 class Movie < Item
   attr_accessor :silet
 
-  
+  def initialize(id, title, archived: false, silet: false)
+    super(id, archived: archived)
+    @title = title
+    @silet = silet
+  end
+
+  def can_be_archived?
+    super && !@silet
+  end
+
+  def to_s
+    "Movie: #{@title} (Silent: #{@silet}, Archived: #{@archived})"
+  end
+end
