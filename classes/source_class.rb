3c9febe9b4285649c72b3b06bdc9f01e795bba44
mariegrace <mariegracebmg@gmail.com>
2023-09-26 13:39:20 +0200

    Add properties for source class

--- classes/source_class.rb
+++ classes/source_class.rb
@@ -1,4 +1,19 @@
 class Source
   attr_reader :id, :name
   attr_accessor :items
-  
+
+  def initialize(id, name)
+    @id = id
+    @name = name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.add_source(self) unless item.source == self
+  end
+
+  def to_s
+    "Source: #{name} (ID: #{@id})"
+  end
+end
