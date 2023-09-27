a9fcfca8daeba1a1ced96c109a0fb08f54c272a3
Mahdi Noori <mahdinoori2389@gmail.com>
2023-09-27 10:48:46 +0430

    Fix linter errors

--- classes/genre.rb
+++ classes/genre.rb
@@ -1,13 +1,14 @@
 class Genre
-    attr_accessor :name, :items
+  attr_accessor :name, :items
 
-    def initialize(name)
-        @id = Random.rand(1..10000)
-        @name = name
-        @items = []
-    end
-    def add_item(item)
-        @items.push(item)
-        item.genre = self
-    end
+  def initialize(name)
+    @id = Random.rand(1..10_000)
+    @name = name
+    @items = []
+  end
+
+  def add_item(item)
+    @items.push(item)
+    item.genre(self)
+  end
 end
--- classes/music_album.rb
+++ classes/music_album.rb
@@ -1,21 +1,14 @@
-require_relative '../item' 
+require_relative '../item'
 
 class MusicAlbum < Item
-    attr_accessor :name, :genre, :publish_date, :on_spotify
-  
-    def initialize(name, publish_date, on_spotify: false, genre: [])
-      super(publish_date)
-      @name = name
-      @publish_date = parse_publish_date(publish_date)
-      @on_spotify = on_spotify
-      @genre = genre
-    end
-    def can_be_archived?
-      parent_result = super
-      if parent_result && @on_spotify
-        return true
-      else
-        return false
-      end
-    end 
+  attr_accessor :on_spotify
+
+  def initialize(on_spotify: false)
+    super()
+    @on_spotify = on_spotify
+  end
+
+  def can_be_archived?
+    super && @on_spotify
+  end
 end
