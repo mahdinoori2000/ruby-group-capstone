7eae50f50d0bff7e239179e4dcfecce35d598420
ctokoli <matumasie@gmail.com>
2023-09-28 15:07:48 +0000

    Update book_class.rb

--- classes/book_class.rb
+++ classes/book_class.rb
@@ -1,5 +1,3 @@
-require_relative './author_class'
-require_relative './label_class'
 require_relative '../item'
 
 # this is a class that inherits from Item class
@@ -26,19 +24,3 @@ class Book < Item
     }
   end
 end
-
-# author = Author.new('John Doe', 'kojo')
-# label = Label.new('The Gods are mad', 'red')
-# new_book = Book.new(label, author, 'publisher', '2020-03-02', 'bad')
-# books = [new_book]
-
-# books.each do |book|
-#   puts "Book ID: #{book.id}"
-#   puts "Book Name: #{book.label.title}"
-#   puts "Book Color: #{book.label.color}"
-#   puts "Author: #{book.author.first_name} #{book.author.last_name}"
-#   puts "Publish Date: #{book.publish_date}"
-#   puts "Publisher: #{book.publisher}"
-#   puts "Cover State: #{book.cover_state}"
-#   puts "----------------------"
-# end
