55f9145628e3f0fdd51e4d27a65934c67fe2d88d
ctokoli <matumasie@gmail.com>
2023-09-28 23:06:30 +0000

    Update create_books.rb

--- app-management/create_books.rb
+++ app-management/create_books.rb
@@ -43,3 +43,23 @@ def list_all_books_method(books, authors, labels)
     puts '----------------------'
   end
 end
+
+# list all authors method
+def list_all_authors_method(authors)
+  @authors = authors
+  @authors.each_with_index do |author, index|
+    print "#{index + 1}] "
+    puts "#{author['first_name']} #{author['last_name']}"
+    puts '.....................'
+  end
+end
+
+# list all labels method
+def list_all_labels_method(labels)
+  @labels = labels
+  @labels.each_with_index do |label, index|
+    print "#{index + 1}] "
+    puts "Title: #{label['title']} | Color: #{label['color']}"
+    puts '.....................'
+  end
+end
