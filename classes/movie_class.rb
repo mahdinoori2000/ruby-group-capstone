7dfa2d07b5293dd33bfea3afe3dce76f6a854b28
mariegrace <mariegracebmg@gmail.com>
2023-09-28 17:38:15 +0200

    Add the tests for movies

--- classes/movie_class.rb
+++ classes/movie_class.rb
@@ -3,7 +3,7 @@ require_relative './author_class'
 require_relative './source_class'
 
 class Movie < Item
-  attr_accessor :title, :silet, :genre, :label
+  attr_accessor :title, :silet, :genre, :label, :archived
 
   def initialize(title, silet, genre, label, publish_date)
     super(publish_date: publish_date)
@@ -15,18 +15,16 @@ class Movie < Item
   end
 
   def can_be_archived?
-    super || @silet
+    @silet || @publish_date < Date.today
   end
 
   def move_to_archive
     return unless can_be_archived?
-
     @archived = true
   end
 
   def to_h
     {
-      id: @id,
       title: @title,
       silet: @silet,
       genre: @genre,
--- spec/movie_spec.rb
+++ spec/movie_spec.rb
@@ -1,22 +1,63 @@
 require_relative '../classes/movie_class'
+require_relative '../classes/author_class'
+require_relative '../classes/source_class'
 
 describe Movie do
-  # Define test cases here
+  before(:each) do
+    @publish_date = Date.new(2023, 2, 12)
+    @movie = Movie.new('Movie Title', false, 'Action', 'Movie Label', @publish_date)
+  end
 
-  describe '#can_be_archived?' do
-    it 'returns true if silent is true' do
-      movie = Movie.new(1, 'Sample Movie', silet: true)
-      expect(movie.can_be_archived?).to be(true)
-    end
+  it 'has a title' do
+    expect(@movie.title).to eq('Movie Title')
+  end
 
-    it 'returns true if the parent method returns true' do
-      movie = Movie.new(1, 'Sample Movie', publish_date: Date.new(2000, 1, 1))
-      expect(movie.can_be_archived?).to be(true)
-    end
+  it 'is not silent by default' do
+    expect(@movie.silet).to be(false)
+  end
 
-    it 'returns false if neither silent nor the parent method is true' do
-      movie = Movie.new(1, 'Sample Movie')
-      expect(movie.can_be_archived?).to be(false)
-    end
+  it 'has a genre' do
+    expect(@movie.genre).to eq('Action')
+  end
+
+  it 'has a label' do
+    expect(@movie.label).to eq('Movie Label')
+  end
+
+  it 'can be archived if it is silent' do
+    @movie.silet = true
+    expect(@movie.can_be_archived?).to be(true)
+  end
+
+  it 'can be archived if it is past the publish date' do
+    @publish_date = Date.new(2021, 1, 1)
+    @movie = Movie.new('Movie Title', false, 'Action', 'Movie Label', @publish_date)
+    expect(@movie.can_be_archived?).to be(true)
+  end
+
+  it 'cannot be archived if it is not silent and not past the publish date' do
+    expect(@movie.can_be_archived?).to be(false)
+  end
+
+  it 'can be moved to archive if it can be archived' do
+    @movie.silet = true
+    @movie.move_to_archive
+    expect(@movie.archived).to be(true)
+  end
+
+  it 'cannot be moved to archive if it cannot be archived' do
+    @movie.move_to_archive
+    expect(@movie.archived).to be(false)
+  end
+
+  it 'returns a hash representation' do
+    expected_hash = {
+      title: 'Movie Title',
+      silet: false,
+      genre: 'Action',
+      publish_date: @publish_date,
+      label: 'Movie Label'
+    }
+    expect(@movie.to_h).to eq(expected_hash)
   end
 end
