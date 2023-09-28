fba9758bf6e5aadcbd7cd7028cce711ad3cd2ae5
Mahdi Noori <mahdinoori2389@gmail.com>
2023-09-28 21:52:43 +0430

    Add previous code to the app.rb file.

--- app.rb
+++ app.rb
@@ -1,8 +1,14 @@
 require_relative 'app-management/ui_class'
+require_relative 'classes/music_album'
+require_relative 'classes/genre'
+require_relative 'app-management/save_data'
+
 
 class App
   def initialize
     @ui = UI.new
+    @music_albums = read_file('./data-files/music_albums.json')
+    @genres = read_file('./data-files/genres.json')
   end
 
   def run
@@ -48,6 +54,18 @@ class App
 
   def list_all_music_albums
     puts 'You have selected 2 - List all music albums'
+    puts '#################'
+    puts '# Music albums: #'
+    puts '#################'
+    @music_albums.each_with_index do |music_album, index|
+      name = "#{index + 1}) Album Name: #{music_album["name"]}"
+      publish_date = " | publish date: #{music_album["publish_date"]}"
+      on_spotify = " | On Spotify: #{music_album["on_spotify"] ? 'Yes' : 'No'}"
+      current_genre = @genres[index]
+      genre = " | Genre: #{current_genre['name']}"
+      album_info = name + publish_date + on_spotify + genre
+      puts album_info
+    end
   end
 
   def list_all_movies
@@ -60,6 +78,9 @@ class App
 
   def list_all_genres
     puts 'You have selected 5 - List all genres'
+    @genres.each_with_index do |genre, index|
+      puts "#{index + 1}. #{genre["name"]}"
+    end
   end
 
   def list_all_labels
@@ -80,6 +101,21 @@ class App
 
   def create_music_album
     puts 'You have selected 10 - Create a music album'
+    puts '##############################'
+    puts '# Enter music album details: #'
+    puts '##############################'
+    name = get_user_input('Enter album title: ')
+    publish_date = get_user_input('Enter date of release (YYYY-MM-DD): ')
+    on_spotify = get_user_input('Is it on Spotify? (Y/N): ').downcase == 'y'
+    genre = get_user_input('Enter the genre of the music album: ').downcase
+    album = MusicAlbum.new(name, publish_date, on_spotify: on_spotify)
+    new_genre = Genre.new(genre)
+    @genres.push(new_genre)
+    @music_albums.push(album)
+    puts "Album '#{album.name}' has been added."
+    puts 'You have selected 10 - Create a music album'
+    save_file(@music_albums, './data-files/music_albums.json')
+    save_file(@genres, './data-files/genres.json')
   end
 
   def create_movie
