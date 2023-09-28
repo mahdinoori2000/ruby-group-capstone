d6334b1e2f203045f22046a75e1991ca43ca3263
Mahdi Noori <mahdinoori2389@gmail.com>
2023-09-28 21:50:10 +0430

    Update music_album file.

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -1,14 +1,25 @@
 require_relative '../item'
 
 class MusicAlbum < Item
-  attr_accessor :on_spotify
+  attr_accessor :name, :genre, :publish_date, :on_spotify
 
-  def initialize(on_spotify: false)
-    super()
+  def initialize(name, publish_date, on_spotify: false)
+    super(publish_date: publish_date)
+    @name = name
+    @publish_date = publish_date
     @on_spotify = on_spotify
   end
 
   def can_be_archived?
     super && @on_spotify
   end
+  
+  def to_h
+    {
+      name: @name,
+      publish_date: @publish_date,
+      on_spotify: @on_spotify
+    }
+  end
+
 end
