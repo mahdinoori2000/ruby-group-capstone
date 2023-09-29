748c91a868c36f9497ecdef5a80e4007138ea99c
Mahdi Noori <mahdinoori2389@gmail.com>
2023-09-29 17:04:13 +0430

    Create music_album table

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,8 +1,17 @@
 --Create database
 CREATE DATABASE catalog;
+
 -- Create item table
 CREATE TABLE item (
   id INT PRIMARY KEY,
   publish_date DATE,
+  archived BOOLEAN,
+);
+
+-- Create music albums table
+CREATE TABLE music_albums (
+  id INT PRIMARY KEY,
+  publish_date DATE NOT NULL DEFAULT DATE,
+  on_spotify BOOLEAN,
   archived BOOLEAN,
 );
