c4740d075f6c0504a79873b986729d2f01009a6a
Mahdi Noori <mahdinoori2389@gmail.com>
2023-09-29 18:45:31 +0430

    Add tables

--- schema/schema.sql
+++ schema/schema.sql
@@ -21,3 +21,41 @@ CREATE TABLE genre (
   id INT PRIMARY KEY,
   name VARCHAR(100)
 );
+
+-- Create book table
+CREATE TABLE book (
+  publisher VARCHAR(100),
+  cover_state BOOLEAN,
+  publish_date DATE
+);
+
+-- Create label table
+CREATE TABLE label (
+  id INT PRIMARY KEY,
+  title VARCHAR(100),
+  color VARCHAR(100)
+);
+
+CREATE TABLE game (
+  multiplayer VARCHAR(100),
+  last_played_at DATE
+);
+
+CREATE TABLE source (
+  id INT PRIMARY KEY,
+  name VARCHAR(100)
+);
+
+CREATE TABLE movie (
+title VARCHAR(50),
+silet BOOLEAN,
+genre VARCHAR(100),
+label VARCHAR(100),
+publish_date DATE
+);
+
+CREATE TABLE author (
+  id INT PRIMARY KEY,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50)
+);
