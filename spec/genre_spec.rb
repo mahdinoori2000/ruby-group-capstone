244df180e2d0291fb8548b2997334820e1cb6149
Mahdi Noori <mahdinoori2389@gmail.com>
2023-09-29 09:59:42 +0430

    Add tests for Genre class

---
+++ spec/genre_spec.rb
@@ -0,0 +1,20 @@
+require_relative '../classes/music_album'
+require_relative '../classes/genre'
+
+describe Genre do
+  let(:genre) { Genre.new('Pop') }
+
+  describe '#initialize' do
+    it 'should sets the name correctly' do
+      expect(genre.name).to eq('Pop')
+    end
+
+    it 'initializes the items array' do
+      expect(genre.items).to be_an(Array)
+    end
+
+    it 'should generate an ID' do
+      expect(genre.instance_variable_get(:@id)).to be_an(Integer)
+    end
+  end
+end
